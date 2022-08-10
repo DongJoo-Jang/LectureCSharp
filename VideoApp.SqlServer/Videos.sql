66f6d3abfc977bc25caef2a8cb28848b19651182
DongJooJang <svra0945@gmail.com>
2022-08-10 12:05:47 +0900

    VideoApp 강의 로컬 DB 게시

---
+++ VideoApp.SqlServer/Videos.sql
@@ -0,0 +1,14 @@
+﻿--비디오 테이블
+CREATE TABLE [dbo].[Videos]
+(
+	[Id] int Not null Identity(1, 1) Primary key,
+	[Created] DateTimeOffset(7)
+		Default(SysDateTimeOffset() AT TIME ZONE 'Koread Standard Time'),
+	
+	[Title] NVarChar(255) Not Null,				--제목
+	[Url] NVarChar(Max) Null,					--URL
+
+	[Nmae] NVarChar(50) Null,					--등록자
+	[Company] NVarChar (255) Null ,				--플랫폼
+)
+Go
